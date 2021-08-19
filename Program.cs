ba62d3c09ccf23dcb63182f48b109da93e598514
student <student@itpark.ru>
2021-08-19 14:16:31 +0300

    first comit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_course_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello git");
+        }
+    }
+}
